836c22830cf007158065d82e7713d684fe2db870
Lucas-Fiche <lucas.fiche.u.borges@gmail.com>
2024-08-28 22:54:41 -0300

    Novas Atualizações

---
+++ alura_curso2-java/ex_02/principalLivro.java
@@ -0,0 +1,9 @@
+package ex_02;
+
+public class principalLivro {
+    public static void main(String[] args){
+        Livro livro1 = new Livro("Harry Potter e a Pedra Filosofal", "J.K Rowling");
+
+        livro1.exibirDetalhes();
+    }
+}
